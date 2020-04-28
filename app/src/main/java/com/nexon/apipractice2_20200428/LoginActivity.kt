10d1cac5851c3d9abb10a2493f6c37e7ddf9dbae
RobertHan96 <yshan4329@gmail.com>
2020-04-28 20:31:06 +0900

    로그인 성공시 토큰 추출

--- app/src/main/java/com/nexon/apipractice2_20200428/LoginActivity.kt
+++ app/src/main/java/com/nexon/apipractice2_20200428/LoginActivity.kt
@@ -2,7 +2,11 @@ package com.nexon.apipractice2_20200428
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.nexon.apipractice2_20200428.utils.ConectServer
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -15,6 +19,29 @@ class LoginActivity : BaseActivity() {
 
     override fun setupEvents() {
         loginBtn.setOnClickListener {
+            val id = idEdt.text.toString()
+            val pw = pwEdt.text.toString()
+
+            ConectServer.postRequestLogin(mContext, id, pw, object : ConectServer.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    Log.d("결과", json.toString())
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        val data = json.getJSONObject("data")
+                        val user = data.getJSONObject("user")
+                        val token = data.getJSONObject("token")
+
+
+                    } else {
+                        val message = json.getString("message")
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            })
 
         }
 
