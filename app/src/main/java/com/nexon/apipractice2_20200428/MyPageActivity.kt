950444b6fafbae1e2015706a6fb473001c158d9e
RobertHan96 <yshan4329@gmail.com>
2020-04-28 22:10:53 +0900

    로그아웃 처리 구현 - 저장된 퇴근 리셋 - 최종 로그아웃 여부는 AlertDialog 활용

--- app/src/main/java/com/nexon/apipractice2_20200428/MyPageActivity.kt
+++ app/src/main/java/com/nexon/apipractice2_20200428/MyPageActivity.kt
@@ -1,7 +1,10 @@
 package com.nexon.apipractice2_20200428
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.nexon.apipractice2_20200428.utils.ContextUtil
 import com.nexon.apipractice2_20200428.utils.GlobalData
 import kotlinx.android.synthetic.main.activity_my_page.*
 
@@ -14,6 +17,20 @@ class MyPageActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        logoutBtn.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃")
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", {dialog, which ->
+                ContextUtil.setUserToken(mContext, "")
+//            로그아웃 (토근 삭제)시 로그인 화면으로 이동
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+        }
 
     }
 
