b370a9588cf805e08d866f962150f56dd73df6c0
RobertHan96 <yshan4329@gmail.com>
2020-04-28 19:43:09 +0900

    Initial commit

---
+++ app/src/main/java/com/nexon/apipractice2_20200428/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.nexon.apipractice2_20200428
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/nexon/apipractice2_20200428/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.nexon.apipractice2_20200428
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
